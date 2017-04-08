7e25302b9809e0b9e89653000bab894a50d01e40
DQDN-20160614BJ\Administrator <huluoboxu0804@gmail.com>
2017-04-08 17:14:03 +0800

    备份

--- application/admin/controller/Chapter.php
+++ application/admin/controller/Chapter.php
@@ -6,8 +6,19 @@ use think\Loader;
 use think\Session;
 use think\Request;
 class Chapter extends Base {
-    public function sublist() {
+    public function index() {
+        if (Request::instance()->get('sid')) {
+            return $this->fetch();
+        }
+
         $subjects = model('Subject')->subjects();
-        
+        if ($subjects) {
+            $lists = $subjects->toArray();
+		    $page = $subjects->render();
+            $trees = sortOut($lists['data'],-1,0,'&nbsp;&nbsp;&nbsp;');
+		    $this->assign('lists',$trees);
+		    $this->assign('page', $page);
+        }
+        return $this->fetch('subject'); 
     }
 }

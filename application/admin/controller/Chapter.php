a0ec38a08c66983fe2f4392057859e3982594f6c
DQDN-20160614BJ\Administrator <huluoboxu0804@gmail.com>
2017-04-22 16:21:14 +0800

    章节页面和url构造，修改了入口文件位置

--- application/admin/controller/Chapter.php
+++ application/admin/controller/Chapter.php
@@ -7,7 +7,8 @@ use think\Session;
 use think\Request;
 class Chapter extends Base {
     public function index() {
-        if (Request::instance()->get('sid')) {
+        $this->assign('action',Request::instance()->action());
+        if (Request::instance()->param('sid')) {
             return $this->fetch();
         }
 
@@ -21,4 +22,22 @@ class Chapter extends Base {
         }
         return $this->fetch('subject'); 
     }
+	
+	public function add() {
+        $this->assign('action',Request::instance()->action());
+		if (Request::instance()->param('sid')) {
+            return $this->fetch();
+        }
+        $subjects = model('Subject')->subjects();
+        if ($subjects) {
+            $lists = $subjects->toArray();
+		    $page = $subjects->render();
+            $trees = sortOut($lists['data'],-1,0,'&nbsp;&nbsp;&nbsp;');
+		    $this->assign('lists',$trees);
+		    $this->assign('page', $page);
+        }
+        return $this->fetch('subject'); 
+
+	}
+	
 }
